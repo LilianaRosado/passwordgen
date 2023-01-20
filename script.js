59cccbf20d5789b975b732a8e5a99c787b0144d9
LilianaRosado <115516854+LilianaRosado@users.noreply.github.com>
2023-01-19 20:03:49 -0700

    Added HTML and JS for new HTML

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-// Assignment code here
+const resultEl = document.getElementById('password');
+const lengthEl = document.getElementById ('length');
+const uppercaseEl = document.getElementById ('uppercase');
+const lowercaseEl = document.getElementById ('lowercase');
+const numbersEl = document.getElementById ('numbers');
+const symbolsEl = document.getElementById ('symbols');
+const generateEl = document.getElementById ('generate');
 
-// Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-//prompt length (more than 8 less than 128)
-//prompt char types to include (lowercase, uppercase, numeric, special numbers)
-var passwordEl = document.getElementById('password');
-var lengthEl;
-
-let length = prompt('Length of Password');
-length = +lengthEl;
-
-console.log(typeof length)
-
-
-//Getting random lower case letters using https://net-comber.com/charset.html
 function randLow(){
 return String.fromCharCode(Math.floor(Math.random() * 26)+ 97);
 }
